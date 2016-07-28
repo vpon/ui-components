8931fb145ed28d402fd4dbea1bc0e70a838554d2
Xuhao <hao.xu@vpon.com>
2016-07-28 11:50:09 +0800

    Expose Resizer in top entrance

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import DataTable from './components/DataTable';
 import DateRange from './components/DateRange';
+import Resizer from './components/Resizer';
 import Helpers from './utils/Helpers';
 
 export default {
   Helpers: Helpers,
   DataTable: DataTable,
-  DateRange: DateRange
+  DateRange: DateRange,
+  Resizer: Resizer
 };
